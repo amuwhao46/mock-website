df189527681b79f518c812e047396b5d5e4fbe24
Oke Amuwha <amuwhao46@gmail.com>
2023-01-03 22:30:32 -0500

    Added footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import ForestPage from './components/ForestPage';
 import MountainPage from './components/MountainPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HomePage />
       <ForestPage />
       <MountainPage />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='flex bg-black text-white pr-5 pt-5 pl-5 pb-12'>
+      <div className='mr-6'>
+        <h1 className='text-3xl font-bold'>Created by</h1>
+        <p className='text-lg'>Oke Amuwha</p>
+      </div>
+      <div>
+        <h1 className='text-3xl font-bold'>Websites</h1>
+        <p className='text-lg'>okeamuwha.com</p>
+      </div>
+    </div>
+  )
+}
